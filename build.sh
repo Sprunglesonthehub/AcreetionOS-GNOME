d099ddb82a2acb5a44fff5877baccf3317420856
Johnathan Spiva <sprungles.me@tuta.io>
2025-01-16 23:08:04 -0800

    Added performance optimizations, nettworking protection, and gesture control

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
- rm -rf work/ && rm -rf out/ && sudo /home/johnathan/Projects/AcreetionOS-GNOME/mkarchiso -L AcreetionOS-GNOME -v -o ../ISO . -j$nproc && sudo rm -rf ./work
+ rm -rf work/ && rm -rf out/ && sudo /home/johnathan/drives/Projects/AcreetionOS-GNOME/mkarchiso -L AcreetionOS-GNOME -v -o ../ISO . -j$nproc && sudo rm -rf ./work
 
