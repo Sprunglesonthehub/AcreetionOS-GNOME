2e4200cfaf4cbdca3a140f22c1e6993b95215d91
Johnathan Spiva <sprungles.me@tuta.io>
2025-02-08 15:17:46 -0800

    Updated Browser as well as some other important information, plus new kernel!

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
- rm -rf work/ && rm -rf out/ && sudo /home/johnathan/drives/Projects/AcreetionOS-GNOME/mkarchiso -L AcreetionOS-GNOME -v -o ../ISO . -j$nproc && sudo rm -rf ./work
+ rm -rf work/ && rm -rf out/ && sudo '/run/media/johnathan/00e9d4db-14eb-4e08-9ac8-d714335eb7ac/Projects/AcreetionOS-GNOME/mkarchiso'  -L AcreetionOS-GNOME -v -o ../ISO . -j$nproc && sudo rm -rf ./work
 
--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="AcreetionOS GNOME"
+iso_name="AcreetionOS-GNOME"
 iso_label="acreetionOS_gnome_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Acreetion OS"
 iso_application="Acreetion OS GNOME Install Media"
