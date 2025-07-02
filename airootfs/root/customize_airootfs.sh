22177f0e426233e7b4060bb85058d331ad6b18af
Johnathan Spiva <sprungles.me@tuta.io>
2025-07-02 03:21:56 -0700

    latest version (initramfs broken)

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -26,18 +26,18 @@ yes | pacman -Scc
 # --- NEW SECTION: Move Calamares autostart file into place ---
 # This runs *after* packages are installed, so the directory exists.
 # The -p flag ensures the directory exists without causing an error if it's already there.
-#!/bin/bash
-set -e
+##!/bin/bash
+#set -e
 
 # Clean up any broken or old kernel references
-rm -rf /lib/modules/6.12.35-1-lts || true
+#rm -rf /lib/modules/6.12.35-1-lts || true
 
 # Disable hostonly mode for dracut
-mkdir -p /etc/dracut.conf.d
-echo 'hostonly="no"' > /etc/dracut.conf.d/disable-hostonly.conf
+#mkdir -p /etc/dracut.conf.d
+#echo 'hostonly="no"' > /etc/dracut.conf.d/disable-hostonly.conf
 
 # Install required packages without triggering auto initramfs
-DRACUT_NO_IMAGE="yes" pacman -Sy --noconfirm linux-lts linux-lts-headers dracut broadcom-wl
+#DRACUT_NO_IMAGE="yes" pacman -Sy --noconfirm linux-lts linux-lts-headers dracut broadcom-wl
 
 # Get the current kernel version
 KVER=$(ls /lib/modules | grep lts | head -n1)
