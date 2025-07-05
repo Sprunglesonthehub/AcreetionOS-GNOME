8b1b5f71b7b4e5e49214a54bcceec0092336e62c
Natalie Spiva <sprungles.me@proton.me>
2025-07-05 07:30:06 -0700

    New versiono networking

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-sudo rm -rf /var/cache/pacman/* && sudo rm -rf work/ && mkarchiso -v -w work -o out .
 if [ "-f /usr/bin/ | grep mkarchiso" != "true" ] ; then
   sudo pacman -Syy mkarchiso --noconfirm
 fi
