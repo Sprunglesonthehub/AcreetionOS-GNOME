9c000bf9ffac25b55436d59073d266a310bc0a7f
Natalie Spiva <162942051+Sprunglesonthehub@users.noreply.github.com>
2025-07-05 07:26:21 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-sudo rm -rf /var/cache/pacman/* && sudo rm -rf work/ && mkarchiso -v -w work -o out . -- -T tmpfs -s 16G
-=======
+sudo rm -rf /var/cache/pacman/* && sudo rm -rf work/ && mkarchiso -v -w work -o out .
 if [ "-f /usr/bin/ | grep mkarchiso" != "true" ] ; then
   sudo pacman -Syy mkarchiso --noconfirm
 fi
